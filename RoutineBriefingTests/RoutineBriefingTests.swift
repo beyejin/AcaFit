ef797783a2f45374b54962dcee397e28ba5b9404
han-yejin <biziyejin8844@naver.com>
2026-06-15 11:54:16 +0900

    Initial Commit

---
+++ RoutineBriefingTests/RoutineBriefingTests.swift
@@ -0,0 +1,18 @@
+//
+//  RoutineBriefingTests.swift
+//  RoutineBriefingTests
+//
+//  Created by 한예진 on 6/15/26.
+//
+
+import Testing
+
+struct RoutineBriefingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
